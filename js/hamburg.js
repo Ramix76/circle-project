1f07bddfea5c3a745cd55b036f34a0b13ce84d46
Ramix76 <framos-p@student.42barcelona.com>
2025-07-08 10:41:06 +0200

    responsive main page done

---
+++ js/hamburg.js
@@ -0,0 +1,12 @@
+const hamburgerBtn = document.getElementById('hamburger-btn');
+const navMenu = document.getElementById('nav-menu');
+
+if (hamburgerBtn && navMenu) {
+  hamburgerBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+  });
+}
+
+
+
+
